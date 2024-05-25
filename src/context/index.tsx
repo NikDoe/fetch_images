dc5254f97eae94be62143467b3cb5a235e244f13
NikDoe <nikd0e@yandex.ru>
2024-05-25 12:28:39 +0300

    check dark mode prefers in script

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,30 +1,41 @@
-import { Dispatch, ReactNode, SetStateAction, createContext, useState } from 'react';
+import { Dispatch, ReactNode, SetStateAction, createContext, useEffect, useState } from 'react';
 
 type GlobalContextState = {
 	query: string;
-    setQuery: Dispatch<SetStateAction<string>>;
-    isDarkTheme: boolean;
-    toggleTheme: () => void;
+	setQuery: Dispatch<SetStateAction<string>>;
+	isDarkTheme: boolean;
+	toggleTheme: () => void;
 };
 
 type Props = {
 	children: ReactNode;
 };
 
+const LOCAL_STORAGE_DARK_MODE = 'dark-mode-theme';
+
 export const GlobalContext = createContext<GlobalContextState | undefined>(
 	undefined
 );
 
+const getInitialDarkMode = () => {
+	const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches;
+	const storedDarkMode = localStorage.getItem(LOCAL_STORAGE_DARK_MODE) === 'true';
+	return storedDarkMode || prefersDarkMode;
+};
 
 const GlobalContextProvider = ({ children }: Props) => {
 	const [query, setQuery] = useState('office');
-	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
 
 	const toggleTheme = () => {
 		const newIsDarkTheme = !isDarkTheme;
 		setIsDarkTheme(newIsDarkTheme);
-		document.body.classList.toggle('dark-theme', newIsDarkTheme);
+		localStorage.setItem(LOCAL_STORAGE_DARK_MODE, String(newIsDarkTheme));
 	};
+
+	useEffect(() => {
+		document.body.classList.toggle('dark-theme', isDarkTheme);
+	}, [isDarkTheme]);
     
 	return (
 		<GlobalContext.Provider value={{ query, setQuery, isDarkTheme, toggleTheme }}>
